2e3ca39e75bb459859baab865a23c86f8a53b0e4
Vellaboyinapradeepkumarreddy <vellaboyinapradeepkumarreddy@gmail.com>
2026-08-19 12:02:46 +0530

    state example 1

--- Day/Day 13/day-13-react-app/src/App.jsx
+++ Day/Day 13/day-13-react-app/src/App.jsx
@@ -2,12 +2,15 @@ import { Welcome } from "./components/P1";
 import { ProductCard } from "./components/P2";
 import headphones from "./assets/sony.jpg";
 import ButtonDemo from "./components/P3";
+import Counter from "./components/StateExampleOne";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Welcome name="Rakesh" />
+      <Counter />
+      
+      <Welcome name="Pradeep" />
 
       <ButtonDemo />
 
